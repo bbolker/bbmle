c0a45a26c4a6a648eb5544c9f604a76d9e28bbf7
Ben Bolker <bbolker@gmail.com>
2018-11-27 17:21:33 -0500

    hacking impsamp [skip ci]

--- R/impsamp.R
+++ R/impsamp.R
@@ -22,7 +22,11 @@ pop_pred_samp <- function(object,
                      PDify=FALSE,
                      PDmethod=NULL,
                      tol = 1e-6,
+                     return_all=FALSE,
+                     rmvnorm_method=c("mvtnorm","MASS"),
                      fix_params=NULL) {
+
+    rmvnorm_method <- match.arg(rmvnorm_method)
     
     min_eval <- function(x) {
         ev <- eigen(x,only.values=TRUE)$values
@@ -47,7 +51,9 @@ pop_pred_samp <- function(object,
     fixed_pars <- setdiff(names(object@fullcoef),names(cc))
     res <- matrix(NA,nrow=n,ncol=length(cc_full),
                   dimnames=list(NULL,names(cc_full)))
-    if (any(is.na(cc))) return(res)
+    if (any(is.na(cc))) return(res)  ## bail out if coefs are NA
+
+    ## try to fix bad covariance matrices
     bad_vcov <- any(is.na(vv))
     if (!bad_vcov) {
         min_eig <- min_eval(vv)
@@ -67,8 +73,10 @@ pop_pred_samp <- function(object,
             warning("NA values in Hessian set to zero: check results *very* carefully!") 
             hh[is.na(hh)] <- 0 # !! questionable
         }
-        if ((is.null(PDmethod) && min_eval(hh)>(-tol)) ||
+        if ((is.null(PDmethod) && bad_vcov) ||
             identical(PDmethod,"King")) {
+            ## ONLY use this
+            warning("using EXPERIMENTAL King et al method")
             ## semi definite: use King et al method
             ## (we may need this when semidefinite, because
             ##  we couldn't invert H in the first place ... nearPD
@@ -80,33 +88,43 @@ pop_pred_samp <- function(object,
             vv <- as.matrix(Matrix::nearPD(vv)$mat)
         }
     }
-    mv_n <- if (impsamp) n_imp else n
-    res[,names(cc)] <- mv_vals <- MASS::mvrnorm(mv_n,mu=cc,Sigma=vv)
+
+    mv_n <- if (impsamp) n_imp else n  ## take more samples if actually sampling
+
+    ## draw MVN samples
+    res[,names(cc)] <- mv_vals <- switch(rmvnorm_method,
+                                         mvtnorm=mvtnorm::rmvnorm(mv_n, mean=cc, sigma=vv),
+                                         MASS=MASS::mvrnorm(mv_n, mu=cc, Sigma=vv)
+    ## fill in fixed parameters as necessary
     if (length(fixed_pars)>0) {
         for (p in fixed_pars) {
             res[,p] <- object@fullcoef[p]
         }
     }
-    if (!(impsamp || return_wts)) return(res)
+    if (!(impsamp || return_wts)) return(res)  ## done
+    
     ## compute MV sampling probabilities
-    mv_wts <- dmvnorm(mv_vals,mu=cc,Sigma=vv,log=TRUE)
-    ## 
+    mv_wts <- mvtnorm::dmvnorm(mv_vals,mean=cc,sigma=vv,log=TRUE)
     if (all(is.na(mv_wts)) && length(mv_wts)==1) {
         ## work around emdbook bug
         mv_wts <- rep(NA,length(mv_vals))
         warning("can't compute MV sampling probabilities")
     }
-    ## compute **log**-likelihoods of each sample point
-    L_wts <- -1*apply(res,1,Lfun)
+    
+    ## compute **log**-likelihoods of each sample point (Lfun is negative LL)
+    L_wts0 <- -1*apply(res,1,Lfun)
     ## shift negative log-likelihoods (avoid underflow);
     ## find scaled likelihood
-    L_wts <- L_wts - mv_wts ## subtract log samp prob
-    L_wts <- exp(L_wts - min(L_wts,na.rm=TRUE))
+    L_wts <- L_wts0 - mv_wts ## subtract log samp prob
+    L_wts <- exp(L_wts - max(L_wts,na.rm=TRUE))
     L_wts <- L_wts/sum(L_wts,na.rm=TRUE)
     eff_samp <- 1/sum(L_wts^2,na.rm=TRUE)  ## check ???
     res <- cbind(res,wts=L_wts)
     attr(res,"eff_samp") <- eff_samp
     ## FIXME: warn if eff_samp is low?
+    if (return_all) {
+        return(cbind(res,loglik=L_wts0,mvnloglik=mv_wts))
+    }
     if (return_wts) return(res)
     ## do importance sampling
     res <- res[sample(seq(nrow(res)),
@@ -116,33 +134,33 @@ pop_pred_samp <- function(object,
     return(res)
 }
     
-## copy (!!) dmvnorm from emdbook to avoid cyclic dependency problems
-dmvnorm <- function (x, mu, Sigma, log = FALSE, tol = 1e-06) {
-    if (is.vector(x)) 
-        x = t(as.matrix(x))
-    n = length(mu)
-    if (is.vector(mu)) {
-        p <- length(mu)
-        if (is.matrix(x)) {
-            mu <- matrix(rep(mu, nrow(x)), ncol = p, byrow = TRUE)
-        }
-    }
-    else {
-        p <- ncol(mu)
-    }
-    if (!all(dim(Sigma) == c(p, p)) || nrow(x) != nrow(mu)) 
-        stop("incompatible arguments")
-    eS <- eigen(Sigma, symmetric = TRUE) 
-    ev <- eS$values
-    if (!all(ev >= -tol * abs(ev[1]))) 
-        warning("Sigma is not positive definite, trying anyway")
-    z = t(x - mu)
-    logdetS = try(determinant(Sigma, logarithm = TRUE)$modulus,
-                  silent=TRUE)
-    if (inherits(logdetS,"try-error")) return(rep(NA,nrow(x)))
-    attributes(logdetS) <- NULL
-    iS = MASS::ginv(Sigma)
-    ssq = diag(t(z) %*% iS %*% z)
-    loglik = -(n * (log(2*pi)) +  logdetS + ssq)/2
-    if (log) loglik else exp(loglik)
-  }
+## ## copy (!!) dmvnorm from emdbook to avoid cyclic dependency problems
+## dmvnorm <- function (x, mu, Sigma, log = FALSE, tol = 1e-06) {
+##     if (is.vector(x)) 
+##         x = t(as.matrix(x))
+##     n = length(mu)
+##     if (is.vector(mu)) {
+##         p <- length(mu)
+##         if (is.matrix(x)) {
+##             mu <- matrix(rep(mu, nrow(x)), ncol = p, byrow = TRUE)
+##         }
+##     }
+##     else {
+##         p <- ncol(mu)
+##     }
+##     if (!all(dim(Sigma) == c(p, p)) || nrow(x) != nrow(mu)) 
+##         stop("incompatible arguments")
+##     eS <- eigen(Sigma, symmetric = TRUE) 
+##     ev <- eS$values
+##     if (!all(ev >= -tol * abs(ev[1]))) 
+##         warning("Sigma is not positive definite, trying anyway")
+##     z = t(x - mu)
+##     logdetS = try(determinant(Sigma, logarithm = TRUE)$modulus,
+##                   silent=TRUE)
+##     if (inherits(logdetS,"try-error")) return(rep(NA,nrow(x)))
+##     attributes(logdetS) <- NULL
+##     iS = MASS::ginv(Sigma)
+##     ssq = diag(t(z) %*% iS %*% z)
+##     loglik = -(n * (log(2*pi)) +  logdetS + ssq)/2
+##     if (log) loglik else exp(loglik)
+##   }
